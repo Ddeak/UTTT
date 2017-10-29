4cd0ff1bfc63862e2041fee5423c339cadcff4b9
ddeak <ddeak@goodpractice.com>
2017-10-29 23:37:38 +0000

    Initial clicks.

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+    public Color color;
+    public Location location;
+    private Renderer rend;
+
+    public enum Location {
+        TopLeft,
+        TopMiddle,
+        TopRight,
+        Left,
+        Middle,
+        Right,
+        BottomLeft,
+        BottomMiddle,
+        BottomRight
+    }
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    private void OnMouseUp()
+    {
+        rend.material.color = Color.blue;
+    }
+}
